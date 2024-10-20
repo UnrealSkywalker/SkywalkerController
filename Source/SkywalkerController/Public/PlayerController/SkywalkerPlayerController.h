0498dd952c170e1eff2f70e0ecc299e307098eb7
Shyfan <410713847@qq.com>
2024-10-20 23:03:11 +0800

    保存

---
+++ Source/SkywalkerController/Public/PlayerController/SkywalkerPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SkywalkerPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYWALKERCONTROLLER_API ASkywalkerPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
