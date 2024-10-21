b4ab39f72680ab696486c2b760edc2f1ee914b5c
Shyfan <410713847@qq.com>
2024-10-21 18:14:24 +0800

    保存

---
+++ Source/SkywalkerController/Public/InputAction/SkywalkerInputAction.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputAction.h"
+#include "SkywalkerInputAction.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYWALKERCONTROLLER_API USkywalkerInputAction : public UInputAction
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SkywalkerController/Public/InputMappingContext/SkywalkerInputMappingContext.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputMappingContext.h"
+#include "SkywalkerInputMappingContext.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYWALKERCONTROLLER_API USkywalkerInputMappingContext : public UInputMappingContext
+{
+	GENERATED_BODY()
+	
+};
